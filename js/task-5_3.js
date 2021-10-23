5f56b68ad9cad64e6dd3fd6d48c7394620ebc2b9
Pavlo-Zubovych <zu10@ukr.net>
2021-10-23 22:11:48 +0300

    update to class

--- js/task-5_3.js
+++ js/task-5_3.js
@@ -6,24 +6,26 @@
 // addItem(item) - получает новый товар и добавляет его к текущим
 // removeItem(item) - получет товар и, если он есть, удаляет его из текущих
 
-const Storage = function ([...items]) {
-  this.items = items;
-};
-
-Storage.prototype.getItems = function () {
-  return this.items;
-};
-
-Storage.prototype.addItem = function (item) {
-  this.items.push(item);
-};
-
-Storage.prototype.removeItem = function (item) {
-  if (this.items.includes(item)) {
-    const deleteIndex = this.items.indexOf(item);
-    this.items.splice(deleteIndex, 1);
+class Storage {
+  constructor([...items]) {
+    this.items = items;
   }
-};
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    if (this.items.includes(item)) {
+      const deleteIndex = this.items.indexOf(item);
+      this.items.splice(deleteIndex, 1);
+    }
+  }
+}
 //
 
 const storage = new Storage([
